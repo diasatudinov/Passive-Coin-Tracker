ff536032d01d404c6c4af5ff0495dbc5bdb6ce00
Dias <dias.atudinov@gmail.com>
2025-12-26 02:46:13 +0500

    add menu view

--- Passive Coin Tracker/Passive Coin Tracker/PCModules/PCMenu/PCMenuView.swift
+++ Passive Coin Tracker/Passive Coin Tracker/PCModules/PCMenu/PCMenuView.swift
@@ -2,17 +2,102 @@
 //  PCMenuView.swift
 //  Passive Coin Tracker
 //
-//  Created by Dias Atudinov on 26.12.2025.
 //
 
 import SwiftUI
 
-struct PCMenuView: View {
+struct PCMenuContainer: View {
+    @AppStorage("firstOpenPC") var firstOpen: Bool = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                AWMenuView()
+                
+            }
+        }
+    }
+}
+
+struct AWMenuView: View {
+    @State var selectedTab = 0
+    @StateObject var viewModel = PCIncomeViewModel()
+    private let tabs = ["My dives", "Calendar", "Stats"]
+    
+    var body: some View {
+        ZStack {
+            
+            switch selectedTab {
+            case 0:
+                PCIncomeView(viewModel: viewModel)
+            case 1:
+                Color.red.ignoresSafeArea()
+            case 2:
+                Color.yellow.ignoresSafeArea()
+            default:
+                Text("default")
+            }
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    ForEach(0..<tabs.count) { index in
+                        Button(action: {
+                            selectedTab = index
+                        }) {
+                            VStack(spacing: 4) {
+                                Image(selectedTab == index ? selectedIcon(for: index) : icon(for: index))
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 24)
+                                
+                                Text(text(for: index))
+                                    .font(.system(size: 16, weight: .medium))
+                                    .foregroundStyle(selectedTab == index ? .accent : .description)
+                            }
+                            .frame(maxWidth: .infinity)
+                            
+                        }
+                    }
+                }
+                .padding(.horizontal, 16).padding(.vertical, 12)
+                .background(.secondaryBlack)
+            }
+            .padding(.bottom, 24)
+            .ignoresSafeArea()
+            
+            
+        }
+    }
+    
+    private func icon(for index: Int) -> String {
+        switch index {
+        case 0: return "tab1IconPC"
+        case 1: return "tab2IconPC"
+        case 2: return "tab3IconPC"
+        default: return ""
+        }
+    }
+    
+    private func selectedIcon(for index: Int) -> String {
+        switch index {
+        case 0: return "tab1IconSelectedPC"
+        case 1: return "tab2IconSelectedPC"
+        case 2: return "tab3IconSelectedPC"
+        default: return ""
+        }
+    }
+    
+    private func text(for index: Int) -> String {
+        switch index {
+        case 0: return "Income"
+        case 1: return "Expenses"
+        case 2: return "Statistics"
+        default: return ""
+        }
     }
 }
 
 #Preview {
-    PCMenuView()
+    AWMenuView()
 }
